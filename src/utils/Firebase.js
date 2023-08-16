39615aa43fbd883c16a0ba48dbb535524ec412fc
Yusuf Yiğit Aydemir <yusufyigitaydemir@gmail.com>
2023-08-16 11:15:04 +0300

    firebase auth fixed

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -4,11 +4,12 @@ import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
-  authDomain: "tradingviewclone.firebaseapp.com",
-  projectId: "tradingviewclone",
-  storageBucket: "tradingviewclone.appspot.com",
-  messagingSenderId: "891226567895",
-  appId: "1:891226567895:web:ec6304d24efd3b30ef2c92"
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MSG_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
